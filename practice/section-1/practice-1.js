91cffc641c3a8b0731665630d93b6414948b3bad
yangjingsheng <673630886@qq.com>
2019-08-19 17:55:15 +0800

    practice

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  // let temp_list = $.extend([],collectionA)
+  // collectionA.forEach(function(value){
+  //   if ($.inArray(value,collectionB)==-1){
+  //     temp_list.splist($.inArray(value,temp_list),1)
+  //   }
+  // })
+  // return temp_list;
+
+  // let intersect = new Set([...collectionA].filter( x => collectionB.has(x)));
+  // return intersect
+
+  var c = collectionA.filter(function(v){ return collectionB.indexOf(v) > -1 })
+  return c
 }
